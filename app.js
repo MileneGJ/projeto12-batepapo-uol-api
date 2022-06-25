94f4c5b875ff0597fa0a0ceb83cc8ea50453d6a3
milenegj <milene.jannetti@gmail.com>
2022-06-25 10:33:02 -0500

    Post participants OK

--- app.js
+++ app.js
@@ -1,40 +1,106 @@
 import express from "express";
 import cors from 'cors';
 import dayjs from "dayjs";
+import { MongoClient } from "mongodb";
+import dotenv from 'dotenv';
 
-
+dotenv.config();
 const app = express();
 
-app.use(cors);
+app.use(cors());
+app.use(express.json());
 
-const participants = [];
-const messages = [];
+const mongoClient = new MongoClient(process.env.MONGO_URL);
+let db
 
-app.post("/participants",(req,res)=>{
-    
+app.post("/participants", async (req, res) => {
+
     //Code for Validation//
 
-    participants.push({
-        name:req.body.name,
-        lastStatus:Date.now()
-    });
-    messages.push({
-        from: req.body.name, 
-        to: 'Todos', 
-        text: 'entra na sala...', 
-        type: 'status', 
-        time: dayjs(new Date(),'HH:mm:ss')
-    });
+    try {
+        await mongoClient.connect();
+        db = mongoClient.db("projeto12Database");
+        db.collection("participants").insertOne({
+            name: req.body.name,
+            lastStatus: Date.now()
+        });
 
-    res.status(201);
+        db.collection("messages").insertOne({
+            from: req.body.name,
+            to: 'Todos',
+            text: 'entra na sala...',
+            type: 'status',
+            time: dayjs(new Date(), 'HH:mm:ss')
+        });
+        res.sendStatus(201);
+        //mongoClient.close();
+    } catch {
+        res.sendStatus(500);
+        //mongoClient.close();
+    }
 });
 
-app.get("/participants",(_,res)=>{
-    res.send(participants)
+app.get("/participants", async (_, res) => {
+    try {
+        await mongoClient.connect();
+        db = mongoClient.db("projeto12Database");
+        const AllUsers = await db.collection("participants").find({}).toArray()
+        res.send(AllUsers);
+        //mongoClient.close();
+    } catch {
+        res.sendStatus(500);
+        //mongoClient.close();
+    }
 });
 
-app.post("/messages",()=>{})
-app.get("/messages",()=>{})
-app.post("/status",()=>{})
+app.post("/messages", async (req, res) => {
+
+    //Code for Validation//
+
+    try {
+        await mongoClient.connect();
+        db = mongoClient.db("projeto12Database");
+        db.collection("messages").insertOne({
+            from: req.header.user,
+            to: req.body.to,
+            text: req.body.text,
+            type: req.body.type,
+            time: dayjs(new Date(), 'HH:mm:ss')
+        });
+        res.sendStatus(201);
+        //mongoClient.close();
+    } catch {
+        res.sendStatus(500);
+        //mongoClient.close();
+    }
+});
+
+
+app.get("/messages", async (req, res) => {
+    const limit = parseInt(req.query.limit);
+    let messagesToSend
+
+    try {
+        await mongoClient.connect();
+        db = mongoClient.db("projeto12Database");
+        const AllMessages = await db.collection("messagens").find({}).toArray()
+
+        if (limit) {
+            messagesToSend = AllMessages.slice(limit, (AllMessages.length - 1))
+        } else {
+            messagesToSend = AllMessages
+        }
+        res.send(messagesToSend);
+        //mongoClient.close();
+    } catch {
+        res.sendStatus(500);
+        //mongoClient.close();
+    }
+});
+
+
+app.post("/status", (req,res) => { 
+    res.send("OK")
+})
 
 app.listen(5000);
